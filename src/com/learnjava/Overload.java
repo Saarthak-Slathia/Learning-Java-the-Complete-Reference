038752f4103a528e25b3943f8f45e465105ee17c
Saarthak-Slathia <saarthak.slathia@gmail.com>
2022-06-04 08:52:48 +0530

    Automatic Type Conversion applied to method overloading

--- src/com/learnjava/Overload.java
+++ src/com/learnjava/Overload.java
@@ -5,33 +5,26 @@ class OverloadDemo{
         System.out.println("No parameters");
     }
 
-    // Overload test for one integer parameter
-    void test(int a){
-        System.out.println("a: " + a);
-    }
-
     // Overload test for two integer parameters
     void test(int a, int b){
         System.out.println("a and b: " + a + " " + b);
     }
 
     // Overload test for a double parameter
-    double test(double a){
-        System.out.println("double a: " + a);
-        return a*a;
+    void test(double a){
+        System.out.println("Inside test(double) a:  " + a);
     }
 }
 
 public class Overload {
     public static void main(String[] args) {
         OverloadDemo ob = new OverloadDemo();
-        double result;
+        int i = 88;
 
-        // Call all versions of test
         ob.test();
-        ob.test(10);
         ob.test(10, 20);
-        result = ob.test(123.25);
-        System.out.println("Result of ob.test(123.25): " + result);
+
+        ob.test(i); // This will invoke test(double)
+        ob.test(123.2); // This will invoke test(double)
     }
 }
