547ea896cf626460f6c90b1af24f5e3f739a6ec5
Saarthak-Slathia <saarthak.slathia@gmail.com>
2022-06-05 08:13:09 +0530

    Static methods and variables

---
+++ src/com/learnjava/UseStatic.java
@@ -0,0 +1,21 @@
+package com.learnjava;
+
+public class UseStatic {
+    static int a = 3;
+    static int b;
+
+    static void meth(int x){
+        System.out.println("X = " + x);
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+    }
+
+    static{
+        System.out.println("Static block initialised");
+        b = a*4;
+    }
+
+    public static void main(String[] args) {
+        meth(42);
+    }
+}
