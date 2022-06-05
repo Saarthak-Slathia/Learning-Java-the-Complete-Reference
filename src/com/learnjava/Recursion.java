e77cf40f2de4c80411567022c974a2d271182e75
Saarthak-Slathia <saarthak.slathia@gmail.com>
2022-06-05 07:41:12 +0530

    Recursion

---
+++ src/com/learnjava/Recursion.java
@@ -0,0 +1,20 @@
+package com.learnjava;
+
+class Factorial{
+    // This is a recursive function
+    int fact(int n){
+        int result;
+        if (n == 1) return 1;
+        result = fact(n-1) * n;
+        return result;
+    }
+}
+
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println("The factorial of 3 is: " + f.fact(3));
+        System.out.println("The factorial of 4 is: " + f.fact(4));
+        System.out.println("The factorial of 5 is: " + f.fact(5));
+    }
+}
